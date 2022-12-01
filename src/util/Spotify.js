9c084310996bdcbd52ccd72d0fd9b6e22e83f0e7
GnartP <gperry4735@gmail.com>
2022-12-01 11:56:03 -0600

    App is functional.Resolved errors in authentication. Song list styling needs adjustment. After saving playlist, it does not reset to 'New Playlist'

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,6 +1,6 @@
-const clientId = process.env.REACT_APP_CLIENT_ID;
+const clientId =process.env.REACT_APP_CLIENT_ID;
 const redirectUri = 'http://localHost:3000/';
-let accessToken = process.env.REACT_APP_API_KEY;
+let accessToken;
 
 const Spotify = {
     getAccessToken() {
@@ -9,27 +9,25 @@ const Spotify = {
         } 
 
         // check for access token match
-        const accessTokenMatch = window.location.href.match(/accessToken=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
         if(accessTokenMatch && expiresInMatch) {
             accessToken = accessTokenMatch[1];
             const expiresIn = Number(expiresInMatch[1]);
-            // This clears paraeters, allowing us to grab a new access token when it expires.
+            // This clears parameters, allowing us to grab a new access token when it expires.
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
             return accessToken;
         } else {
-            const accessUrl = `https://accounts.spotify.com/authorize?
-            client_id=${clientId}&response_type=token&
-            scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
             window.location = accessUrl;
         }
     },
     search(term) {
         const accessToken = Spotify.getAccessToken();
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
-        { headers: {
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, { 
+            headers: {
             Authorization: `Bearer ${accessToken}`
             }
         }).then(response => {
@@ -42,25 +40,25 @@ const Spotify = {
                 id: track.id,
                 name: track.name,
                 artist: track.artists[0].name,
-                album: track.ablum.name,
+                album: track.album.name,
                 uri: track.uri
             }));
         })
     },
-    savePlaylist(name,trackUris) {
+    savePlaylist(name, trackUris) {
         if (!name || !trackUris.length) {
             return;
         }
 
         const accessToken = Spotify.getAccessToken();
-        const headers = { Authorization: `Bearer ${accessToken}`};
+        const headers = { Authorization: `Bearer ${accessToken}` };
         let userId;
 
         return fetch('https://api.spotify.com/v1/me', { headers: headers }
         ).then(response => response.json()
         ).then(jsonResponse => {
             userId = jsonResponse.id;
-            return fetch(`https://api.spotify.com//v1/users/${userId}/playlists`,
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
             {
                 headers: headers,
                 method: 'POST',
@@ -68,11 +66,12 @@ const Spotify = {
             }).then(response => response.json()
             ).then(jsonResponse => {
                 const playlistId = jsonResponse.id;
-                return fetch(`https://api.spotify.com//v1/users/${userId}/playlists/${playlistId}/tracks`), {
+                console.log(headers)
+                return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
                     headers: headers,
                     method: 'POST',
                     body: JSON.stringify({ uris: trackUris })
-                }
+                })
             })
         })
     }
