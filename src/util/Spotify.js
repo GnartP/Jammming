4a499a1e2273aa9f196ec99e0b4fcca2c8136a7d
GnartP <gperry4735@gmail.com>
2022-12-01 12:14:02 -0600

    Used Surge to deploy app

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId =process.env.REACT_APP_CLIENT_ID;
-const redirectUri = 'http://localHost:3000/';
+const redirectUri = 'http://spot_jam.surge.sh';
 let accessToken;
 
 const Spotify = {
